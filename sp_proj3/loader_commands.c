89047a43327cdcdff77735762a884261567dddc1
AlphaTechnic <alphatechnicist@gmail.com>
2021-04-22 21:13:14 +0900

    Write code that works when reading D record, and E record

--- sp_proj3/loader_commands.c
+++ sp_proj3/loader_commands.c
@@ -15,7 +15,6 @@ OK_or_ERR loader(char filename){
 void load_pass1(FILE *fp){
     char *ptr;
     char line[LINE_LEN];
-    char cs_name[LINE_LEN];
     char addr_str[LINE_LEN];
     int addr_int;
 
@@ -23,19 +22,50 @@ void load_pass1(FILE *fp){
         fgets(fp, LINE_LEN, line);
         if (line[0] == 'H'){
             // get CS name
+            char cs_name[LINE_LEN];
+
             ptr = strtok(line, " ");
-            ptr ++;
+            ptr++;
             strcpy(cs_name, ptr);
             ptr = strtok(NULL, " ");
 
             // get starting addr
-            strncpy(addr_str, ptr, 2*3);
-            addr_str[2*3] = '\0';
+            strncpy(addr_str, ptr, 6);
+            addr_str[6] = '\0';
             addr_int = hexstr_to_decint(addr_str);
 
             TOTAL_LEN += CS_LEN;
             printf("%-7s  %-7s  %04X     %04X   \n", cs_name, " ", CS_ADDR + addr_int, CS_LEN);
         }
+        else if (line[0] == 'D'){
+            char symbol_name[LINE_LEN];
 
+            ptr = strtok(line, " ");
+            ptr++;
+            while(1){
+                // get name of symbol
+                if (strlen(ptr) > 6){
+                    strncpy(symbol_name, ptr, 6);
+                    symbol_name[6] = '\0';
+                    ptr += 6;
+                }
+                else{
+                    strcpy(symbol_name, ptr);
+                    ptr = strtok(NULL, " ");
+                }
+
+                // get address of symbol
+                strncpy(addr_str, ptr, 6);
+                addr_str[6] = '\0';
+                addr_int = hexstr_to_decint(addr_str);
+
+                // es_table에 push
+
+                printf("%-7s  %-7s  %04X   \n", " ", symbol_name, CS_ADDR + addr_int);
+            }
+        }
+        else if (line[0] == 'E'){
+            CS_ADDR += CS_LEN;
+        }
     }
 }
